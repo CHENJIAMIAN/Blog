49f7ed94aafb93d75a6d88433b16fab50f442861
chenjiamian <571574085@qq.com>
2023-05-27 12:54:33 +0800

    test

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { getSideBar } from 'vitepress-plugin-autobar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base:'',
+  base:'CHENJIAMIAN',
   lang: 'zh',
   title: "CHENJIAMIAN",
   description: "个人博客",
