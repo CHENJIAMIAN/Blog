82dec46f4f3c1d32954a0f8e2d11eddad6185f11
chenjiamian <571574085@qq.com>
2023-12-04 12:01:11 +0800

    fix bug

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,23 +5,13 @@ import LayoutIndex from './layout/Index.vue'
 
 import 'gitalk/dist/gitalk.css'
 
-let lastRoutePath = ''; // 用于存储上一个路由的路径
-
 export default {
   ...DefaultTheme,
   Layout: LayoutIndex,
   enhanceApp({ router }) {
     if (inBrowser) {
-      router.beforeEach((to, from, next) => {
-        lastRoutePath = from.path; // 更新上一个路由的路径
-        next();
-      });
-
       router.onAfterRouteChanged = () => {
-        // 检查路由是否完全不同
-        if (router.currentRoute.path !== lastRoutePath) {
-          busuanzi.fetch();
-        }
+        busuanzi.fetch()
       }
     }
   }
